b40f17a24be7630409692e4641ef76055459f85c
Lorena Sales <lorenassales@gmail.com>
2023-06-06 11:59:30 +0000

    created dim_products and dim_customers

---
+++ models/marts/dim_products.sql
@@ -0,0 +1,46 @@
+with
+    products as (
+        select *
+        from {{ ref('stg_erp__products') }}
+    )
+
+    , categories as (
+        select *
+        from {{ ref('stg_erp__categories') }}
+    )
+
+    , suppliers as (
+        select *
+        from {{ ref('stg_erp__suppliers') }}
+    )
+
+    , join_charts as (
+        select
+            products.product_id
+            , categories.category_id
+            , suppliers.supplier_id
+            , products.product_name
+            , products.quantity_per_unit
+            , products.unit_price
+            , products.units_in_stock
+            , products.units_on_order
+            , products.reorder_level
+            , categories.category_name
+            , suppliers.supplie_company_name
+            , suppliers.supplie_city
+            , suppliers.supplie_country       
+        from products
+        left join categories on
+            products.category_id = categories.category_id
+        left join suppliers on
+            products.supplier_id = suppliers.supplier_id
+    )
+
+    , changes as (
+        select
+            row_number() over (order by product_id) as product_sk
+            , *
+        from join_charts
+    )
+select *
+from changes
--- models/stage/erp/stg_erp__suppliers.sql
+++ models/stage/erp/stg_erp__suppliers.sql
@@ -7,11 +7,12 @@ with
     , changes as (
         select
             cast(supplier_id as int) as supplier_id
-            , cast(company_name as string) as company_name
-            , cast(contact_name as string) as contact_name
-            , cast(contact_title as string) as contact_title
-            , cast(address as string) as address
-            , cast(city as string) as city
+            , cast(company_name as string) as supplie_company_name
+            , cast(contact_name as string) as supplie_contact_name
+            , cast(contact_title as string) as supplie_contact_title
+            , cast(address as string) as supplie_address
+            , cast(city as string) as supplie_city
+            , cast(country as string) as supplie_country
         from suppliers
     )
 select *
