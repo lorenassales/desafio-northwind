5b96d77175469c4e4d8da441dcee9986bb69af0d
Lorena Sales <lorenassales@gmail.com>
2023-06-06 14:38:32 +0000

    created dim_employee

--- models/stage/erp/stg_erp__employees.sql
+++ models/stage/erp/stg_erp__employees.sql
@@ -7,14 +7,14 @@ with
     , changes as (
         select
             cast(employee_id as int) as employee_id
+            , cast(reports_to as int) as manager_id
             , cast((last_name || " " || first_name) as string) as employee_name
-            , cast(title as string) as role_of
+            , cast(title as string) as role_of            
             , cast(hire_date as date) as hire_date
             , cast(address as string) as address
             , cast(city as string) as city
             , cast(region as string) as region
-            , cast(country as string) as country
-            , cast(reports_to as int) as reports_to
+            , cast(country as string) as country           
         from employees
     )
 select *
